b016125c78cd41a4302a6f101aee5bf7aa99ed37
sandeb <sandrine.debatty@gmail.com>
2020-06-26 01:23:45 +0200

    angenda with CSS

--- migrations/Version20200625201251.php
+++ migrations/Version20200625201251.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200625082028 extends AbstractMigration
+final class Version20200625201251 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,12 +20,12 @@ final class Version20200625082028 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE prescription ADD medications LONGTEXT NOT NULL');
+        $this->addSql('ALTER TABLE pharmacy ADD medications LONGTEXT DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE prescription DROP medications');
+        $this->addSql('ALTER TABLE pharmacy DROP medications');
     }
 }
